9fce9833366fef497a60e854bb51f006f2ca8926
Thais oliveira <thaislecinha@gmail.com>
2021-10-01 12:12:13 -0300

    projeto teste

---
+++ GiteGitHubTeste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GiteGitHubTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste");
+            Console.ReadLine();
+        }
+    }
+}
